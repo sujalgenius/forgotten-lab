a0fdca406484c01d9044bf20c179b5941ef7fa13
sujalgenius <sujalgenius@gmail.com>
2026-06-29 09:56:22 +1000

    add working dialogue box system with show/hide on NPC interaction
    add working dialogue box system with show/hide on NPC interaction

--- forgotten-lab/scripts/dialogue_box.gd
+++ forgotten-lab/scripts/dialogue_box.gd
@@ -5,5 +5,5 @@ func _ready():
 	style.bg_color = Color(0.635, 0.809, 0.494, 1.0)
 	style.border_width_top = 2
 	style.border_color = Color(0.917, 0.757, 0.961, 1.0)
-	add_theme_color_override("panel", style)
+	add_theme_stylebox_override("panel", style)
 	hide()
